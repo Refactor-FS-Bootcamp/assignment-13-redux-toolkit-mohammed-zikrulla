5dcc0aa40cad9de7dfb4800e276cc9a3005ba55f
mohammed-zikrulla <zikrulla.zik@gmail.com>
2023-01-01 15:11:29 +0530

    assignment-13

---
+++ redux-toolkit-project/src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "fce3796fb174435d87e2ee65673f11e5";
---
+++ redux-toolkit-project/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>Movie App</div>
+      <div>©2021, Mohammed Zikrulla</div>
+    </div>
+  );
+};
+
+export default Footer;
